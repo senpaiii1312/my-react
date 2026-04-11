72d672b4200ceafa421318a9c11d94c67b260df6
senpaiii1312 <spatil13122004@gmail.com>
2026-04-11 13:35:35 +0530

    accodian component updated

--- accordian-component/src/App.js
+++ accordian-component/src/App.js
@@ -25,20 +25,30 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordianItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordianItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordianItem>
       ))}
     </div>
   );
 }
 
-function AccordianItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordianItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -46,7 +56,7 @@ function AccordianItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
